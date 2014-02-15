7f5d800f734f998b809d2829243774697216ec4b
Jani Patokallio <jpatokal@iki.fi>
2014-02-15 22:48:31 +1100

    Switch to Google Charts API

--- public/js/opal_or_not.js
+++ public/js/opal_or_not.js
@@ -1,3 +1,9 @@
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(initChart);
+
+var chart;
+var options;
+
 $('form').on('click', 'button.transfer', function() {
 	$('.segment-2').toggleClass('hidden');
   $('.transfer.btn').toggleClass('hidden');
@@ -25,5 +31,38 @@ $('form').on('click', 'button.compare', function() {
   setTimeout(function() {
     $('.spinner').addClass('hidden');
     $('.results').removeClass('hidden');
+    drawChart();
   }, 1000);
 });
+
+function initChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Ticket', 'Weekly cost', { role: 'style' }, { role: 'annotation' } ],
+    ['Opal', 10, 'gray', '$0.00' ],
+    ['MyMulti', 10, 'gray', '$0.00' ],
+    ['TravelTen', 10, 'gray', '$0.00' ],
+    ['Weekly', 10, 'gray', '$0.00' ],
+  ]);
+  chart = new google.visualization.BarChart(document.getElementById('bar-chart'));
+  options = {
+    animation: { duration: 500 },
+    fontName: 'Georgia,"Times New Roman",Times,serif',
+    fontSize: 20,
+    hAxis: { gridlines: { count: 0 }, minValue: 0, ticks: [] },
+    legend: { position: "none" }
+  };
+  chart.draw(data, options);
+  // Chart cannot be drawn on hidden div
+  $('.results').addClass('hidden');
+}
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Ticket', 'Weekly cost', { role: 'style' }, { role: 'annotation' } ],
+    ['Opal', 28.94, 'gray', '$28.94' ],
+    ['MyMulti', 20.49, 'gray', '$20.49' ],
+    ['TravelTen', 19.30, '#4582EC', '$19.30 ✓' ],
+    ['Weekly', 21.45, 'gray', '$21.45' ],
+  ]);
+  chart.draw(data, options);
+}
