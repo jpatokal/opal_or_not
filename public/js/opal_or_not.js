32a229c30666cd4b3832f5fb934da5e29de2bf94
Jani Patokallio <jpatokal@iki.fi>
2014-02-15 22:05:32 +1100

    Try out bar charts via Bootstrap progress bars

--- public/js/opal_or_not.js
+++ public/js/opal_or_not.js
@@ -1,4 +1,29 @@
 $('form').on('click', 'button.transfer', function() {
-	$('.transfer-div').toggleClass('hidden');
+	$('.segment-2').toggleClass('hidden');
   $('.transfer.btn').toggleClass('hidden');
+});
+
+$('form').on('click', 'button.compare', function() {
+  $('.spinner').removeClass('hidden');
+  $('.results').addClass('hidden');
+
+  var data = [
+    { "mode": $('form .segment-1 .mode').val(),
+      "zone": $('form .segment-1 .zone').val(),
+      "count": $('form .count').val() }
+  ];
+  console.log(! $('.segment-2').hasClass('hidden'));
+  if(! $('.segment-2').hasClass('hidden')) {
+    data.push({
+      "mode": $('form .segment-2 .mode').val(),
+      "zone": $('form .segment-2 .zone').val(),
+      "count": $('form .count').val()
+    });
+  }
+  console.log(JSON.stringify(data));
+
+  setTimeout(function() {
+    $('.spinner').addClass('hidden');
+    $('.results').removeClass('hidden');
+  }, 1000);
 });
