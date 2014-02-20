887193896c174f396fe0dd085016a1211ee7ca1c
Jani Patokallio <jpatokal@iki.fi>
2014-02-20 23:23:52 +1100

    Add stations to distance to zones logic

--- public/js/opal_or_not.js
+++ public/js/opal_or_not.js
@@ -4,12 +4,17 @@ google.setOnLoadCallback(initChart);
 var chart;
 var options;
 var submit_button_html = $('form button.compare').html();
+var directionsService = new google.maps.DirectionsService();
 
 // Open/close second trip segment
 $('form').on('click', 'button.transfer', function() {
 	$('.segment-2').toggleClass('hidden');
   $('.transfer.btn').toggleClass('hidden');
 });
+
+function hasTransfer() {
+  return (! $('.segment-2').hasClass('hidden'));
+}
 
 // Update zone options when mode changes
 function selectModeHandler(segment) {
@@ -31,13 +36,31 @@ selectModeHandler('.segment-2');
 $('form').on('click', 'button.compare', function() {
   $('button.compare').html('Calculating... <i class="fa fa-spinner fa-spin"/>').attr('disabled', true);
 
+  if($('form .segment-1 .mode').val() == 'train') {
+    getTrainDistance(
+      $('form .segment-1 select.origin').val(),
+      $('form .segment-1 select.destination').val(), 1);
+    return; // async
+  } else {
+    if(hasTransfer && $('.segment-2 .mode').val() == 'train') {
+      getTrainDistance(
+        $('form .segment-2 select.origin').val(),
+        $('form .segment-2 select.destination').val(), 2);
+      return; // async 
+    }
+  }
+  // else
+  doSubmit();
+});
+
+function doSubmit() {
   var mode = $('form .segment-1 .mode').val();
   var data = [
     { "mode": mode,
       "zone": $('form .segment-1 .zone.' + mode).val(),
       "count": $('form .count').val() }
   ];
-  if(! $('.segment-2').hasClass('hidden')) {
+  if(hasTransfer()) {
     mode = $('form .segment-2 .mode').val();
     data.push({
       "mode": mode,
@@ -67,7 +90,45 @@ $('form').on('click', 'button.compare', function() {
   jqXhr.always( function(data) {
       $('button.compare').html(submit_button_html).removeAttr('disabled');
     });
-});
+}
+
+// This would make more sense to do in the backend, but Google's quotas are
+// much more generous for client-side JS requests.
+function getTrainDistance(origin, destination, segment) {
+  var request = {
+      origin: origin + " train station, Sydney, NSW",
+      destination: destination + " train station, Sydney, NSW",
+      transitOptions: {
+        departureTime: new Date()
+      },
+      travelMode: google.maps.TravelMode.TRANSIT
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      distanceToTrainZone(response.routes[0].legs[0].distance.value / 1000, segment);
+    } else {
+      // TODO something...
+      console.error(status, response);
+    }
+  });
+}
+
+function distanceToTrainZone(distance, segment) {
+  var zone;
+  if(distance < 10) {
+    zone = 1;
+  } else if (distance > 10 && distance < 20) {
+    zone = 2;
+  } else if (distance > 20 && distance < 35) {
+    zone = 3;
+  } else if (distance > 35 && distance < 65) {
+    zone = 4;
+  } else {
+    zone = 5;
+  }
+  $('form .segment-' + segment + ' .zone.train').val(zone);
+  doSubmit();
+}
 
 function initChart() {
   var initial_data = [
