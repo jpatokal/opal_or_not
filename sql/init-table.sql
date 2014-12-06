4653391933b63c819e45ec0afc8d0d1ab9aa585c
Jani Patokallio <jpatokal@iki.fi>
2014-12-06 20:28:21 +1100

    Revert "Add light rail to stats"
    This reverts commit 1c7cfda5b54611d7fe9e193eec4e260310e882ff.

--- sql/init-table.sql
+++ sql/init-table.sql
@@ -5,13 +5,9 @@ CREATE TABLE modes (
 INSERT INTO modes(mode) VALUES
 	('bus'),
 	('bus+ferry'),
-	('bus+light-rail'),
 	('bus+train'),
 	('ferry'),
-	('ferry+light-rail'),
 	('ferry+train'),
-	('light-rail'),
-	('light-rail+train'),
 	('train'),
 	('unknown');
 
--- sql/off-peak.sql
+++
@@ -1,3 +0,0 @@
-select am, pm, sum(count), sum(signed_sum), sum(signed_sum)/sum(count) as avg from (select name, am, pm, count, case when name='Opal' then -sum else sum end as signed_sum from peak_stats) as foo group by am, pm;
-
-select sum(count), sum(signed_sum), sum(signed_sum)/sum(count) as avg from (select name, count, case when name='Opal' then -sum else sum end as signed_sum from opal where mode='train') as foo;
