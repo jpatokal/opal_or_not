564df75924e9835e33fbdcec490da8ff3a44a67b
Jani Patokallio <jpatokal@iki.fi>
2014-12-05 23:49:02 -0800

    Basic light rail fare model

---
+++ models/light_rail.rb
@@ -0,0 +1,72 @@
+class LightRail < Fare
+  def zone
+    raise "Undefined"
+  end
+
+  def fare_table
+    {
+      "light_rail" => {
+        1 => zone[0],
+        2 => zone[1]
+      },
+    }
+  end
+end
+
+class LightRailSingle < LightRail
+  def name
+    "Light Rail Singles"
+  end
+
+  def zone
+    [3.60, 4.60]
+  end
+
+  def compute_segment(segment)
+    if segment[:mode] == "light_rail" and segment[:count] < 8
+      super
+    else
+      nil
+    end
+  end
+end
+
+class LightRailReturn < LightRail
+  def name
+    "Light Rail Returns"
+  end
+
+  def zone
+    [5.00, 6.20]
+  end
+
+  def off_peak?(segment)
+    segment[:mode] == "train" and segment[:time] and segment[:time][:am] == "after"
+  end
+
+  def compute_segment(segment)
+    if segment[:mode] == "light_rail"
+      super / 2  # super is a single fare, not returns
+    else
+      nil
+    end
+  end
+end
+
+class LightRailWeekly < LightRail
+  def name
+    "Light Rail Weekly"
+  end
+
+  def zone
+    [23, 23]
+  end
+
+  def compute_segment(segment)
+    if segment[:mode] == "light_rail" and segment[:count] > 4
+      single segment
+    else
+      nil
+    end
+  end
+end
---
+++ spec/light_rail_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+ 
+describe LightRail do
+  it "returns nil unless there is a light_rail segment" do
+    light_rail = LightRailWeekly.new([
+      {:mode => "bus", :zone => 1, :count => 1},
+    ])
+    light_rail.compute.should be_nil
+  end
+
+  describe LightRailSingle do
+    it "sums up singles by count" do
+      light_rail = LightRailSingle.new([
+        {:mode => "light_rail", :zone => 2, :count => 6},
+      ])
+      light_rail.compute.should be_within(0.01).of(light_rail.fare_table["light_rail"][2] * 6)
+    end
+
+    it "does not offer singles for 4 or more days per week" do
+      LightRailSingle.new([{:mode => "light_rail", :zone => 2, :count => 8}]).compute.should be_nil
+    end
+   end
+
+  describe LightRailReturn do
+    it "sums up singles by half count" do
+      light_rail = LightRailReturn.new([
+        {:mode => "light_rail", :zone => 2, :count => 6},
+      ])
+      light_rail.compute.should be_within(0.01).of(light_rail.fare_table["light_rail"][2] * 3)
+    end
+  end
+
+  describe LightRailWeekly do
+    it "ignores count and looks up weekly fares directly" do
+      light_rail = LightRailWeekly.new([
+        {:mode => "light_rail", :zone => 2, :count => 6},
+      ])
+      light_rail.compute.should == light_rail.fare_table["light_rail"][2]
+    end
+
+    it "does not offer weeklies for 2 or less days per week" do
+      LightRailWeekly.new([{:mode => "light_rail", :zone => 2, :count => 4}]).compute.should be_nil
+    end
+  end
+end
